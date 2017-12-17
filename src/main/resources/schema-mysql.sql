f26c4a22c05faea6c71290a514ed551f688450a8
Andy <andreas.van@smail.inf.h-brs.de>
2017-12-17 22:47:01 +0100

    Using Docker containers for your Spring boot applications https://g00glen00b.be/docker-spring-boot/

---
+++ src/main/resources/schema-mysql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `superhero` (
+  `id`         INT(11)     NOT NULL AUTO_INCREMENT,
+  `name`       VARCHAR(32) NOT NULL,
+  `first_name` VARCHAR(32),
+  `last_name`  VARCHAR(32),
+  `good`       BIT(1),
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
